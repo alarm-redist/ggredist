c3b1ea3fa1da93c38b5bdc1b6e69ddc1a52937ff
Cory McCartan <cory.mccartan@gmail.com>
2022-06-20 17:08:25 -0400

    partisan scales

--- R/party.R
+++ R/party.R
@@ -1,76 +1,142 @@
 #' Party scales for `ggplot2`
 #'
-#' @rdname scale_party
-#' @export
-#' @concept scales
-#'
 #' @param name name for scale. Default is `"Democratic share"`.
 #' @param midpoint Scale midpoint value. Default is `0.5`.
 #' @param limits Lower and upper limits for scale. Default is `0:1`.
 #' @param labels function to adjust scale labels. Default is `scales::percent`.
 #' @param oob function to deal with out of bounds. Default is `scales::squish()`.
+#' @param nice.breaks if `TRUE`, attempt to place breaks at nice values instead
+#'   of exactly evenly spaced between the limits.
 #' @param ... additional arguments to `ggplot::scale_*` functions
 #'
-#'
-#' @md
 #' @examples
-#' scale_fill_party_c()
-#' scale_fill_party_d()
-#' scale_color_party_c()
-#' scale_color_party_c()
-#' scale_color_party_d()
-#' scale_color_party_b()
+#' library(ggplot2)
+#' data(oregon)
+#'
+#' ggplot(oregon, aes(fill = ndv / (ndv + nrv))) +
+#'     geom_sf(size = 0) +
+#'     scale_fill_party_c(limits=c(0.3, 0.7))
+#'     theme_map()
+#'
+#' ggplot(oregon, aes(fill = ndv / (ndv + nrv))) +
+#'     geom_sf(size = 0) +
+#'     scale_fill_party_c(limits=c(0.3, 0.7))
+#'     theme_map()
+#'
+#' @name scale_party
+#' @concept scales
+NULL
+
+#' @rdname scale_party
+#' @export
 scale_fill_party_c <- function(name = 'Democratic share', midpoint = 0.5, limits = 0:1,
                                labels = scales::percent, oob = scales::squish, ...,
                                reverse = FALSE) {
-  ggplot2::scale_fill_gradient2(
-    name = name, ..., low = GOP_DEM[1], high = GOP_DEM[15],
-    midpoint = midpoint, limits = limits, labels = labels, oob = oob
-  )
+  pal = ggredist$partisan
+  if (reverse) pal = rev(pal)
+
+  if (is.null(midpoint)) {
+    rescaler = scales::rescale
+  } else {
+    rescaler = function(x, to=c(0, 1), from=range(x, na.rm=TRUE)) {
+      scales::rescale_mid(x, to, from, midpoint)
+    }
+  }
+
+  ggplot2::scale_fill_gradientn(name = name, limits = limits, labels = labels,
+                                oob = oob, colours=pal, rescaler=rescaler)
 }
 
-#' @rdname scale_party
-#' @export
-scale_fill_party_d <- function(...) {
-  ggplot2::scale_fill_manual(...,
-    values = c(GOP_DEM[2], GOP_DEM[14]),
-    labels = c('Rep.', 'Dem.')
-  )
-}
-
-#' @rdname scale_party
-#' @export
-scale_fill_party_b <- function(...) {
-  ggplot2::binned_scale('fill', 'party',
-                        palette = grDevices::colorRampPalette(GOP_DEM),
-                        ...
-  )
-}
 
 #' @rdname scale_party
 #' @export
 scale_color_party_c <- function(name = 'Democratic share', midpoint = 0.5, limits = 0:1,
                                 labels = scales::percent, oob = scales::squish, ...) {
-  ggplot2::scale_color_gradient2(
-    name = name, ..., low = GOP_DEM[1], high = GOP_DEM[15],
-    midpoint = midpoint, limits = limits, labels = labels, oob = oob
-  )
+  pal = ggredist$partisan
+  if (reverse) pal = rev(pal)
+
+  if (is.null(midpoint)) {
+    rescaler = scales::rescale
+  } else {
+    rescaler = function(x, to=c(0, 1), from=range(x, na.rm=TRUE)) {
+      scales::rescale_mid(x, to, from, midpoint)
+    }
+  }
+
+  ggplot2::scale_color_gradientn(name = name, limits = limits, labels = labels,
+                                 oob = oob, colours=pal, rescaler=rescaler)
 }
 
 #' @rdname scale_party
 #' @export
-scale_color_party_d <- function(...) {
-  ggplot2::scale_color_manual(...,
-    values = c(GOP_DEM[2], GOP_DEM[14]),
-    labels = c('Rep.', 'Dem.')
-  )
+scale_fill_party_d <- function(labels = c('Rep.', 'Dem.'), ...) {
+  pal = c(ggredist$partisan[2], ggredist$partisan[14])
+  if (reverse) pal = rev(pal)
+
+  ggplot2::scale_fill_manual(..., values = pal, labels = labels)
 }
 
 #' @rdname scale_party
 #' @export
-scale_color_party_b <- function(...) {
-  ggplot2::binned_scale('color', 'party',
-    palette = grDevices::colorRampPalette(GOP_DEM),
-    ...
-  )
+scale_color_party_d <- function(labels = c('Rep.', 'Dem.'), reverse = FALSE, ...) {
+  pal = c(ggredist$partisan[2], ggredist$partisan[14])
+  if (reverse) pal = rev(pal)
+
+  ggplot2::scale_color_manual(..., values = pal, labels = labels)
 }
+
+
+#' @rdname scale_party
+#' @export
+scale_fill_party_b <- function(name = 'Democratic share', midpoint = 0.5, limits = 0:1,
+                               labels = scales::percent, oob = scales::squish,
+                               reverse = FALSE, nice.breaks = FALSE, ...) {
+  pal = ggredist$partisan
+  if (reverse) pal$pal = rev(pal)
+  pal_fun = scales::colour_ramp(pal)
+
+  if (is.null(midpoint)) {
+    rescaler = scales::rescale
+  } else {
+    rescaler = function(x, to=c(0, 1), from=range(x, na.rm=TRUE)) {
+      scales::rescale_mid(x, to, from, midpoint)
+    }
+  }
+
+  ggplot2::binned_scale("fill", "party", name = name, limits = limits,
+                        labels = labels, oob = oob, palette = pal_fun,
+                        rescaler = rescaler, nice.breaks = nice.breaks, ...)
+}
+
+#' @rdname scale_party
+#' @export
+scale_color_party_b <- function(name = 'Democratic share', midpoint = 0.5, limits = 0:1,
+                                labels = scales::percent, oob = scales::squish,
+                                reverse = FALSE, nice.breaks = FALSE, ...) {
+  pal = ggredist$partisan
+  if (reverse) pal$pal = rev(pal)
+  pal_fun = scales::colour_ramp(pal)
+
+  if (is.null(midpoint)) {
+    rescaler = scales::rescale
+  } else {
+    rescaler = function(x, to=c(0, 1), from=range(x, na.rm=TRUE)) {
+      scales::rescale_mid(x, to, from, midpoint)
+    }
+  }
+
+  ggplot2::binned_scale("color", "party", name = name, limits = limits,
+                        labels = labels, oob = oob, palette = pal_fun,
+                        rescaler = rescaler, nice.breaks = nice.breaks, ...)
+}
+
+
+#' @rdname scale_party
+#' @export
+scale_colour_party_d = scale_color_party_d
+#' @rdname scale_party
+#' @export
+scale_colour_party_c = scale_color_party_c
+#' @rdname scale_party
+#' @export
+scale_colour_party_b = scale_color_party_b
