2555a34a9fa207f91a642f6cbf4ccf032ac7147b
Cory McCartan <cory.mccartan@gmail.com>
2022-06-20 15:50:10 -0400

    oregon doc

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Oregon Redistricting File
+#'
+#' This data contains geographic, demographic, and political information on the
+#' 1,071 census tracts of the state of Oregon.
+#'
+#' @name oregon
+#' @usage data("oregon")
+#' @format `sf` object
+
+#' \describe{
+#' \item{\code{county}}{The county the tract belongs to.}
+#' \item{\code{cd_2020}}{The 2210 congressional district assignment for the tract.}
+#' \item{\code{pop}}{The total population of the tract, according to the 2020 Census.}
+#' \item{\code{pop_white}}{The non-Hispanic white population of the precinct.}
+#' \item{\code{ndv}}{Average number of votes for Democratic candidates in recent statewide elections.}
+#' \item{\code{nrv}}{Average number of votes for Republican candidates in recent statewide elections.}
+#' \item{\code{geometry}}{The `sf` geometry column containing the geographic information.}
+#' }
+#'
+#' @examples
+#' data(oregon)
+#' print(oregon)
+NULL
