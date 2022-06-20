97cc4e623f663803827af5942a64b82bc201fdf0
Cory McCartan <cory.mccartan@gmail.com>
2022-06-20 16:27:33 -0400

    consolidate into list

--- R/colors.R
+++ R/colors.R
@@ -1,68 +1,31 @@
-#' Party Colors
+#' Redistricting Color Palettes
 #'
-#' Vector of evenly spaced party colors from red to white to blue.
+#' Included palettes:
+#' - **partisan**, a perceptually uniform red-white-blue scale
+#' - **dra**, the Dave's Redistricting App classic color palette
+#' - *penn82**, Historic categorical color palette from the 1982 published Pennsylvania
+#' congressional district map
 #'
-#' @format character vector of length 15
+#' @format list of character vectors of type `palette` (which supports a
+#'   `plot()` generic for visualization)
 #'
-#' @concept colors
 #' @examples
-#' GOP_DEM
+#' plot(ggredist$partisan)
+#' plot(ggredist$dra)
+#' plot(ggredist$ye_olde_pa)
+#'
 #' @export
-GOP_DEM <- c(
-  '#A0442C', '#B25D4C', '#C27568', '#D18E84', '#DFA8A0',
-  '#EBC2BC', '#F6DCD9', '#F9F9F9', '#DAE2F4', '#BDCCEA',
-  '#9FB6DE', '#82A0D2', '#638BC6', '#3D77BB', '#0063B1'
-)
-
-#' Ye Olde Pennsylvania
-#'
-#' Historic categorical color palette for Pennsylvania redistricting.
-#'
-#' @format character vector of length 6
-#'
-#' @concept colors
-#' @examples
-#' ye_olde_pa
-#' @export
-ye_olde_pa <- c('#FDFD63', '#FAD89B', '#B69940', '#FDFCB1', '#F6B10C', '#F9DE60')
-
-
-#' FiveThirtyEight Colors
-#'
-#' Five Thirty Eight's redistricting color scheme for various levels of political
-#' competitiveness.
-#'
-#' @format character vector of length 5
-#'
-#' @concept colors
-#' @examples
-#' fivethirtyeight
-#' @export
-fivethirtyeight <- c('#5768AC', '#A1A9ED', '#EAE3EB', '#FF998A', '#FA5A50')
-
-#' Dave's Redistricting Colors
-#'
-#' Dave's Redistricting App classic color palette.
-#'
-#' @format character vector of length 5
-#'
-#' @concept colors
-#' @examples
-#' dra
-#' @export
-dra <- c(
-  '#7070ff',
-  '#70B870',
-  '#BE70BE',
-  '#FF7070',
-  '#FFE970',
-  '#70B8B8',
-  '#E6AB80',
-  '#ABA2E3',
-  '#70FFFF',
-  '#FF7BC2',
-  '#B7FF70',
-  '#A8C4F5',
-  '#F3C4B4'
+ggredist = list(
+  partisan = structure(c('#A0442C', '#B25D4C', '#C27568', '#D18E84', '#DFA8A0',
+                         '#EBC2BC', '#F6DCD9', '#F9F9F9', '#DAE2F4', '#BDCCEA',
+                         '#9FB6DE', '#82A0D2', '#638BC6', '#3D77BB', '#0063B1'),
+                       class="palette"),
+  dra = structure(c('#7070ff', '#70B870', '#BE70BE', '#FF7070', '#FFE970',
+                    '#70B8B8', '#E6AB80', '#ABA2E3', '#70FFFF', '#FF7BC2',
+                    '#B7FF70', '#A8C4F5', '#F3C4B4'), class="palette"),
+  penn82 = structure(c('#EEEF3F', '#FAD49C', '#AD8A5A', '#FDFCB1',
+                       '#EAA414', '#F7DA6A'), class="palette"),
+  fivethirtyeight = structure(c('#5768AC', '#A1A9ED', '#EAE3EB',
+                                '#FF998A', '#FA5A50'), class="palette")
 )
 
--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,33 @@ make_bins <- function(v, pal, where) {
   force(v)
   pal[as.integer(cut(v, where))]
 }
+
+# `plot.palette` modified from that in `wesanderson` (c) 2016 Karthik Ram
+#' @export
+#' @importFrom graphics rect par image text
+plot.palette = function(x, ...) {
+  n <- length(x)
+  old <- par(mar=c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n), col=x,
+        ylab="", xaxt="n", yaxt="n", bty="n")
+
+  rect(0, 0.9, n + 1, 1.1, col=grDevices::rgb(1, 1, 1, 0.8), border=NA)
+  text((n + 1) / 2, 1, labels=deparse(substitute(x)), col="black", cex=1, font=2)
+
+  if (!is.null(names(x)))
+    text(1:n, 1.25, labels=names(x), col="black", cex=1)
+}
+
+
+
+#' Paste-to-hex
+#' @keywords internal
+#' @noRd
+pth = function(x) {
+  rgb = as.list(as.integer(strsplit(x, "\\s")[[1]]) / 256)
+  col = do.call(colorspace::sRGB, rgb)
+  str = paste0('"', colorspace::hex(col), '"')
+  code_output(str)
+}
