39771e8ef95da4ecf59f2388cd5ff1573cc447e3
Christopher Kenny <ctk44@cornell.edu>
2022-11-17 12:45:44 -0500

    remove the palette class

--- R/colors.R
+++ R/colors.R
@@ -9,39 +9,37 @@
 #' Geographic political maps
 #' - **coast** and **larch**, inspired by natural scenery
 #'
-#' @format list of character vectors of type `palette` (which supports a
-#'   `plot()` generic for visualization)
+#' @format list of character vectors of hex colors
 #'
 #' @examples
-#' plot(ggredist$partisan)
-#' plot(ggredist$dra)
-#' plot(ggredist$penn82)
-#' plot(ggredist$randmcnally)
-#' plot(ggredist$natgeo)
-#' plot(ggredist$coast)
-#' plot(ggredist$larch)
+#' scales::show_col(ggredist$partisan)
+#' scales::show_col(ggredist$dra)
+#' scales::show_col(ggredist$penn82)
+#' scales::show_col(ggredist$randmcnally)
+#' scales::show_col(ggredist$natgeo)
+#' scales::show_col(ggredist$coast)
+#' scales::show_col(ggredist$larch)
 #'
 #' @concept colors
 #' @export
 ggredist = list(
-  partisan = structure(c('#A0442C', '#B25D4C', '#C27568', '#D18E84', '#DFA8A0',
+  partisan = c('#A0442C', '#B25D4C', '#C27568', '#D18E84', '#DFA8A0',
                          '#EBC2BC', '#F6DCD9', '#F9F9F9', '#DAE2F4', '#BDCCEA',
                          '#9FB6DE', '#82A0D2', '#638BC6', '#3D77BB', '#0063B1'),
-                       class="palette"),
-  dra = structure(c('#7070ff', '#70B870', '#BE70BE', '#FF7070', '#FFE970',
+  dra = c('#7070ff', '#70B870', '#BE70BE', '#FF7070', '#FFE970',
                     '#70B8B8', '#E6AB80', '#ABA2E3', '#70FFFF', '#FF7BC2',
-                    '#B7FF70', '#A8C4F5', '#F3C4B4', '#BEBE7E'), class="palette"),
-  penn82 = structure(c('#EDEF3F', '#FED09E', '#A7825A', '#FFF8B7',
-                       '#E2A414', '#ECD166'), class="palette"),
-  randmcnally = structure(c("#f4c450", "#e79274", "#d3a7ae", "#fcf092",
-                            "#98b791", "#c7d4da", "#e8ba8f"), class="palette"),
-  natgeo = structure(c("#abafd0", "#e8b3a5", "#fded7e", "#b6c572",
-                       "#efc965", "#fcf3e2"), class="palette"),
-  coast = structure(c("#D4B46E", "#84AFA2", "#547286",
-                      "#7F7C74", "#B2BCD6", "#526042"), class="palette"),
-  larch = structure(c("#D0A75F", "#626B5D", "#8C8F9E",
-                      "#858753", "#A4BADA", "#CDB7AA"), class="palette"),
-  fivethirtyeight = structure(c('#5768AC', '#A1A9ED', '#EAE3EB',
-                                '#FF998A', '#FA5A50'), class="palette")
+                    '#B7FF70', '#A8C4F5', '#F3C4B4', '#BEBE7E'),
+  penn82 = c('#EDEF3F', '#FED09E', '#A7825A', '#FFF8B7',
+                       '#E2A414', '#ECD166'),
+  randmcnally = c("#f4c450", "#e79274", "#d3a7ae", "#fcf092",
+                            "#98b791", "#c7d4da", "#e8ba8f"),
+  natgeo = c("#abafd0", "#e8b3a5", "#fded7e", "#b6c572",
+                       "#efc965", "#fcf3e2"),
+  coast = c("#D4B46E", "#84AFA2", "#547286",
+                      "#7F7C74", "#B2BCD6", "#526042"),
+  larch = c("#D0A75F", "#626B5D", "#8C8F9E",
+                      "#858753", "#A4BADA", "#CDB7AA"),
+  fivethirtyeight = c('#5768AC', '#A1A9ED', '#EAE3EB',
+                                '#FF998A', '#FA5A50')
 )
 
--- R/utils.R
+++ R/utils.R
@@ -12,21 +12,3 @@ rot_pal <- function(pal) {
     }
   }
 }
-
-# `plot.palette` modified from that in `wesanderson` (c) 2016 Karthik Ram
-#' @export
-#' @importFrom graphics rect par image text
-plot.palette = function(x, ...) {
-  n <- length(x)
-  old <- par(mar=c(0.5, 0.5, 0.5, 0.5))
-  on.exit(par(old))
-
-  image(1:n, 1, as.matrix(1:n), col=x,
-        ylab="", xaxt="n", yaxt="n", bty="n")
-
-  rect(0, 0.9, n + 1, 1.1, col=grDevices::rgb(1, 1, 1, 0.8), border=NA)
-  text((n + 1) / 2, 1, labels=deparse(substitute(x)), col="black", cex=1, font=2)
-
-  if (!is.null(names(x)))
-    text(1:n, 1.25, labels=names(x), col="black", cex=1)
-}
