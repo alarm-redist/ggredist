57ec17c12deb492255f6fb7f07859b0d19226402
Christopher Kenny <ctk44@cornell.edu>
2025-10-01 10:03:32 -0400

    style file

--- R/538.R
+++ R/538.R
@@ -11,26 +11,26 @@
 #' data(oregon)
 #'
 #' ggplot(oregon, aes(fill = ndv / (ndv + nrv))) +
-#'     geom_sf(size = 0) +
-#'     scale_fill_538(name = '') +
-#'     theme_map()
+#'   geom_sf(size = 0) +
+#'   scale_fill_538(name = '') +
+#'   theme_map()
 #' @concept colors
 #' @export
 scale_fill_538 <- function(...) {
   breaks <- c(0, 0.35, 0.45, 0.55, 0.65, 1)
   limits <- c(0.25, 0.75)
-  ivals <-  scales::rescale(breaks, from = limits)
+  ivals <- scales::rescale(breaks, from = limits)
   ggplot2::binned_scale('fill',
-                        palette = function(x) {
-                          ggredist$fivethirtyeight[
-                            findInterval(x, ivals, all.inside = TRUE, rightmost.closed = TRUE)
-                          ]
-                        },
-                        breaks = breaks,
-                        limits = limits,
-                        oob = scales::squish,
-                        guide = 'colourbar',
-                        ...
+    palette = function(x) {
+      ggredist$fivethirtyeight[
+        findInterval(x, ivals, all.inside = TRUE, rightmost.closed = TRUE)
+      ]
+    },
+    breaks = breaks,
+    limits = limits,
+    oob = scales::squish,
+    guide = 'colourbar',
+    ...
   )
 }
 
@@ -40,18 +40,17 @@ scale_fill_538 <- function(...) {
 scale_color_538 <- function(...) {
   breaks <- c(0, 0.35, 0.45, 0.55, 0.65, 1)
   limits <- c(0.25, 0.75)
-  ivals <-  scales::rescale(breaks, from = limits)
+  ivals <- scales::rescale(breaks, from = limits)
   ggplot2::binned_scale('color',
-                        palette = function(x) {
-                          ggredist$fivethirtyeight[
-                            findInterval(x, ivals, all.inside = TRUE, rightmost.closed = TRUE)
-                          ]
-                        },
-                        breaks = breaks,
-                        limits = limits,
-                        oob = scales::squish,
-                        guide = 'colourbar',
-                        ...
+    palette = function(x) {
+      ggredist$fivethirtyeight[
+        findInterval(x, ivals, all.inside = TRUE, rightmost.closed = TRUE)
+      ]
+    },
+    breaks = breaks,
+    limits = limits,
+    oob = scales::squish,
+    guide = 'colourbar',
+    ...
   )
 }
-
